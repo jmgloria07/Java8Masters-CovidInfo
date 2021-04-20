0dc6c4b7928e282d119d332e2bf5d5b43b93716b
Josh Gloria <joshuag@magenic.com>
2021-04-20 15:56:04 +0800

    change data store implementation to utilize service do functions

--- src/com/magenic/covidinfo/services/CovidInfoDataStore.java
+++ src/com/magenic/covidinfo/services/CovidInfoDataStore.java
@@ -5,17 +5,5 @@ import java.util.List;
 import com.magenic.covidinfo.models.CovidInfo;
 
 public class CovidInfoDataStore {
-	private static List<CovidInfo> covidInfos;
-	
-	public void add(CovidInfo covidInfo) {
-		
-	}
-	
-	public List<CovidInfo> getAll() {
-		return covidInfos;
-	}
-	
-	public void deleteCovidInfo(String name) {
-		//removeIf
-	}
+	public static List<CovidInfo> covidInfos;
 }
